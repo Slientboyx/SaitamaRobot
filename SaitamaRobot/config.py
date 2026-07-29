0a85e2e1ed02aa2a7e2ba11e1e481cad4d454ca3
Slientboyx <161596052+Slientboyx@users.noreply.github.com>
2026-07-29 10:50:21 +0500

    Modify API_ID, API_HASH, and OWNER_ID in config.py
    Updated API credentials and owner details in config.py.

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -1,3 +1,17 @@
+Slient Boy:
+
+
+
+
+
+
+
+
+
+
+ gc
+
+
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
@@ -14,18 +28,18 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
     ALLOW_CHATS = True
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_ID = "2568615"  # integer value, dont use ""
+    API_HASH = "1e62cca9207a4469ca847526acebb660"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 8980101371  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Sawada"
     SUPPORT_CHAT = "OnePunchSupport"
     # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1003920280238
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1004446276751
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
